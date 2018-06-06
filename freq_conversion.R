ad21a41b2c1b53cfee9f564cdedd6384c51ecbe9
Mohammed El-Kebir <melkebir@cs.brown.edu>
2018-06-06 17:19:58 -0500

    scripts

---
+++ freq_conversion.R
@@ -0,0 +1,162 @@
+# read in frequency output and convert to .data for spruce to enumerate
+
+# add f average column
+# add mu: (1,0,0) (1,1,1)
+
+library(readr)
+
+rewrite_tree2freq_for_downsample <- function(filename, coverage = 10000, input_folder = './tree2freqs_output/', output_folder = ',/downsample_input/'){
+  output_file <- paste(output_folder, filename, sep = '')
+  file <- read_lines(paste(input_folder, filename, sep = ''))
+  if (length(file) < 2){return()}
+  # file <- file[-c(1)]
+  
+  table <- file[c(5:length(file))]
+  write_lines(table, 'temp.txt')
+  mtx <- read_delim("temp.txt", "\t", escape_double = FALSE, col_names = FALSE, trim_ws = TRUE)
+  colnames(mtx) <- c('sample_index','sample_label','anatomical_site_index','anatomical_site_label','character_index','character_label','ref','var')
+  
+  mtx[,7] <- floor(coverage*(1-mtx[,8]))
+  mtx[,8] <- coverage-mtx[,7]
+  
+  write_tsv(mtx, 'temp.txt')
+  new_file <- read_lines('temp.txt')
+  new_file[1] <- paste('#', new_file[1], sep = '')
+  new_file <- c(file[1:3], new_file)
+  # new_file <- new_file[-c(3)] #paste('#', new_file[3], sep = ' ')
+  write_lines(new_file, output_file)
+  return(mtx)
+}
+
+rewrite_tree2freq_for_downsample_wrapper <- function(M = c(0.1,1,10), S = c(0:30)){
+  for (m in M){
+    for (s in S){
+      file <- paste('freqs_M', m, '_S', s, '.txt', sep = '')
+      # print(file)
+      rewrite_tree2freq_for_downsample(filename = file)
+    }
+  }
+}
+
+rewrite_downsample_for_spruce <- function(freqs_filename, downsample_filename, coverage = 10000, downsample_input_folder = './downsample_output/', freqs_input_folder = './reads2freqs_output/', output_folder = './spruce_input/'){
+  output_file <- paste(output_folder, freqs_filename, sep = '')
+  
+  freqs_file <- read_lines(paste(freqs_input_folder, freqs_filename, sep = ''))
+  if (length(freqs_file) < 2){return()}
+  # freqs_file <- freqs_file[-c(1)]
+  
+  downsample_file <- read_lines(paste(downsample_input_folder, downsample_filename, sep = ''))
+  if (length(downsample_file) < 2){return()}
+  # downsample_file <- downsample_file[-c(1)]
+  
+  table <- downsample_file[c(5:length(downsample_file))]
+  write_lines(table, 'temp.txt')
+  downsample_mtx <- read_delim("temp.txt", "\t", escape_double = FALSE, col_names = FALSE, trim_ws = TRUE)
+  colnames(downsample_mtx) <- c('sample_index','sample_label','anatomical_site_index','anatomical_site_label','character_index','character_label','ref','var')
+  f_avg <- downsample_mtx[,8]/(downsample_mtx[,8]+downsample_mtx[,7])
+  
+  table <- freqs_file[c(5:length(freqs_file))]
+  write_lines(table, 'temp.txt')
+  freqs_mtx <- read_delim("temp.txt", "\t", escape_double = FALSE, col_names = FALSE, trim_ws = TRUE)
+  colnames(freqs_mtx) <- c('sample_index','sample_label','anatomical_site_index','anatomical_site_label','character_index','character_label','f-','f+')
+  sample_idx  <- freqs_mtx[,1]
+  sample_label  <- freqs_mtx[,2]
+  snv_idx   <- freqs_mtx[,5]
+  snv_label <- freqs_mtx[,6]
+  f_minus <- freqs_mtx[,7]
+  f_plus <- freqs_mtx[,8]
+  
+  output_mtx <- freqs_mtx
+  
+  output_mtx[,1] <- sample_idx
+  output_mtx[,2] <- sample_label
+  output_mtx[,3] <- snv_idx
+  output_mtx[,4] <- snv_idx
+  output_mtx[,5] <- f_minus
+  output_mtx[,6] <- f_avg
+  output_mtx[,7] <- f_plus
+  
+  # set x,y,mu
+  output_mtx[,8] <- as.integer(1)
+  output_mtx[,9] <- as.integer(1)
+  output_mtx[,10] <- as.integer(1)
+  
+  colnames(output_mtx) <- c('sample_index','sample_label','character_index','character_label','f-','f','f+','x','y','mu')
+  
+  write_delim(output_mtx, 'temp.txt', delim = " ")
+  new_file <- read_lines('temp.txt')
+  new_file[1] <- paste('#', new_file[1], sep = '')
+  # print(file[1:2])
+  new_file <- c(freqs_file[1:2], new_file)
+  m <- strsplit(new_file[1], split = '#')[[1]][1]
+  n <- strsplit(new_file[2], split = '#')[[1]][1]
+  
+  new_file[1] <- paste(m, '#m', sep = '')
+  new_file[2] <- paste(n, '#n', sep = '')
+  new_file <- new_file[-c(3)] #paste('#', new_file[3], sep = ' ')
+  write_lines(new_file, output_file)
+  # return(mtx)
+}
+
+rewrite_downsample_for_spruce_wrapper <- function(C = c(-1,50,100,1000), P = c(1), K = c(2,5,10), M = c(0.1,1,10), 
+                                                  S = c(0:30)){
+  for (c in C){
+    for (p in P){
+      for (k in K){
+        for (m in M){
+          for (s in S){
+            downsample_file <- paste('downsampled_C', c, '_P', p, '_k', k, '_M', m, '_S', s, '.txt', sep = '')
+            freqs_file <- paste('freq_C', c, '_P', p, '_k', k, '_M', m, '_S', s, '.txt', sep = '')
+            rewrite_downsample_for_spruce(downsample_filename = downsample_file, freqs_filename = freqs_file)
+          }
+        }
+      }
+    }
+  }
+  
+}
+
+num_snv_analysis <- function(M = c(0.1,1,10), S = c(0:30)){
+  n_snv <- matrix(data = NA, ncol = length(M), nrow = length(S))
+  colnames(n_snv) <- M
+  rownames(n_snv) <- S
+  
+  # plot.new()
+  plot(0,0, xlim = c(0, 12), ylim = c(0, 1000))
+  # plot(1,lwd=0,axes=c(0, 10, 0, 1000),xlab="",ylab="",...)
+  
+  for (m in 1:length(M)){
+    for (s in 1:length(S)){
+      file <- paste('freqs_M', M[m], '_S', S[s], '.txt', sep = '')
+      folder <- './tree2freqs_output/'
+      filename <- paste(folder, file, sep = '')
+      lines <- read_lines(filename)
+      if (length(lines) < 4){next}
+      n <- strsplit(lines[3], split = ' #')[[1]][1]
+      n <- as.numeric(n)
+      n_snv[s,m] <- n
+      points(x = M[m], y = n)
+    }
+  }
+  
+  num_snv <- c(n_snv[,1], n_snv[,2], n_snv[,3])
+  print(length(num_snv))
+  m_rate <- c(rep(0.1, 31), rep(1, 31), rep(10, 31))
+  snv_df <- data.frame(rate = as.character(m_rate), n_snv = num_snv)
+  
+  return(snv_df)
+}
+
+# MAIN OPERATIONS
+
+rewrite_tree2freq_for_downsample_wrapper()
+rewrite_downsample_for_spruce_wrapper()
+# snv <- num_snv_analysis()
+# ggplot(snv, aes(x=rate, y=log10(n_snv))) + geom_boxplot() + ggtitle('Mutation Count per Rate')
+# rewrite_downsample_for_spruce('downsampled_C50_P1_k2_M0.1_S0.txt')
+# rewrite_tree2freq_for_downsample_wrapper(M=c(0.1, 1), S = 1:20)
+# output <- rewrite_tree2freq_for_downsample('freqs_M0.1_S0.txt')
+
+
+
+# output <- rewrite_freq_for_spruce('../frequency output/freq1.txt')
--- generate.sh
+++ generate.sh
@@ -1,41 +1,71 @@
 #!/bin/bash
-# run on turing.cs.princeton.edu
 
-if [ ! $# -eq 1 ]
-then
-    echo "Usage: $0 <simulate_executable>" >&2
-    exit 1
-fi
+#Cover=10000
+#
+#for M in {0.1,1,10}
+#do
+#    if [ ! -e simulate_output/M$M ]
+#    then
+#        mkdir simulate_output/M$M
+#    fi
+#    echo simulate_output/M$M
+#    for S in {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30}
+#    do
+#        ./executables/simulate -kP 10 -mut $M -k 0 -m 1 -s $S -P 1 -C $Cover > simulate_output/M$M/sim_S$S.txt
+#	#touch sequence_output/seq_M$M_S$S.txt
+#        ./executables/tree2freqs simulate_output/M${M}/sim_S${S}.txt > tree2freqs_output/freqs_M${M}_S${S}.txt
+#        #./executables/sequence -C $Cover simulate_output/M$M/sim_S$S.txt > sequence_output/seq_M${M}_S${S}.txt
+#    done
+#done
 
-#$1 -kP 2 -k 1 -D 2e-7 -m 4 -p 0 -s 0 -N 10 -C 200 -o . -c ../../coloring.txt
+# run R code to convert freqs to reads
 
-for C in {60,200,10000}
+for C in {-1,50,100,1000}
 do
-    if [ ! -e C${C} ]
-    then
-        mkdir C${C}
-    fi
-    for P in {0.6,0.8,1}
+    for P in 1
     do
-        if [ ! -e C${C}/P${P} ]
-        then
-            mkdir C${C}/P${P}
-        fi
-	for k in {1,3,5}
-	do
-            if [ ! -e C${C}/P${P}/k${k} ]
-            then
-                mkdir C${C}/P${P}/k${k}
-            fi
-            for s in {0,10,12,2,3,4,5,7,8,9}
+        for k in {2,5,10}
+        do
+            for M in {0.1,1,10}
             do
-                if [ $k -eq 1 ]
-                then
-                    $1 -kP 2 -k 1 -D 2e-7 -m 4 -p 0 -s $s -C $C -E 0.001 -P $P -o C${C}/P${P}/k${k} -c ../../coloring.txt
-		else
-                    $1 -kP $k -k $k -D 2e-7 -m 4 -p 0 -s $s -C $C -E 0.001 -P $P -o C${C}/P${P}/k${k} -c ../../coloring.txt
-                fi
+                for S in {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30}
+                do
+                    if [ -s downsample_input/freqs_M${M}_S${S}.txt ]
+                    then
+                        ./executables/downsample -P $P -k $k -C $C downsample_input/freqs_M${M}_S${S}.txt > downsample_output/downsampled_C${C}_P${P}_k${k}_M${M}_S${S}.txt
+                    fi
+                    if [ -s downsample_output/downsampled_C${C}_P${P}_k${k}_M${M}_S${S}.txt ]
+                    then
+                        ./executables/reads2freqs downsample_output/downsampled_C${C}_P${P}_k${k}_M${M}_S${S}.txt > reads2freqs_output/freq_C${C}_P${P}_k${k}_M${M}_S${S}.txt
+                    fi
+                done
             done
-	done
+        done
     done
 done
+
+# run r code to convert downsampled reads to freqs for spruce input
+
+#for C in {-1,50,100,1000}
+#do
+#    for P in 1
+#    do
+#        for k in {2,5,10}
+#        do
+#            for M in {0.1,1,10}
+#            do
+#                for S in {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30}
+#                do
+#                    #./executables/enumerate spruce_input/downsampled_C${C}_P${P}_k${k}_M${M}_S${S}.txt > enumerate_output/sim_C${C}_P${P}_k${k}_M${M}_S${S}.txt
+#                    #./executables/parse enumerate_output/sim_C${C}_P${P}_k${k}_M${M}_S${S}.txt
+#                done
+#            done
+#        done
+#    done
+#done 
+
+#./parse ../result/n_15_noisy/sims_r0_m10_n15_c1000.res 
+
+#./recall ../result/n_15_noisy/sims_r0_m10_n15_c1000.res ../data/sims/n_15_noisy/sims_r0_m10_n15_c1000.true >> trees/sims_r0_m10_n15_c1000_recall.csv
+# can spruce use oncosim tree as input for measuring recall?
+
